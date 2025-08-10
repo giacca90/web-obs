2b6160b64dd7b6a916be3320d7ea8e3d8e93f232
Giacca90 <giacca90mobile@gmail.com>
2025-08-10 17:40:39 +0200

    Corregido error de estilos en los botones de presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   content: ["projects/web-obs/**/*.{html,ts}"],
+  safelist: [
+    "grid-cols-[repeat(auto-fill,minmax(45\\%,1fr))]",
+    "max-h-[33vh]",
+    "aspect-video",
+    "!aspect-video",
+  ],
   theme: {
     extend: {},
   },
